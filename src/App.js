3fbccf9c4e5356763e61fef09cf70b8cd07d6c98
Hoangvu1872002 <hoangvu1872k2@gmail.com>
2023-05-15 21:40:24 +0700

    push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from "react";
+import Login from "./component/loginRegister/Login"
+import Register from "./component/loginRegister/Register"
+import ListUser from './component/loginRegister/ListUser'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+// import Menu from './component/menu/Menu';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+    
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/Register" element={<Register />}/> 
+        <Route path="/ListUser" element={<ListUser />}/>        
+      </Routes>
+    </Router>
+    {/* <Menu></Menu> */}
+    </>
   );
 }
 
 export default App;
+
+
