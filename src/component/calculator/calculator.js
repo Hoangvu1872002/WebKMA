2625fc215ad172f9e23f24686c8fe735063a4163
Hoangvu1872002 <hoangvu1872k2@gmail.com>
2023-05-16 01:49:55 +0700

    push

--- src/component/calculator/calculator.js
+++ src/component/calculator/calculator.js
@@ -1,26 +1,21 @@
 window.onload = function () {
-var input = document.querySelector('.input');
+  var input = document.querySelector(".input");
 
-console.log(input);
-var items = Array.from(document.querySelectorAll(".item"));
-console.log(items);
-items.forEach(function(btn){
-  btn.addEventListener('click',function(){
-    if(input.innerHTML === '0')
-    input.innerHTML = "";
-    if(btn.innerHTML === 'AC')
-    input.innerHTML = '0';
-    else if(btn.innerHTML === "DEL"){
-      var arrText =Array.from(input.innerHTML);
-      arrText.splice(arrText.length-1,1);
-      if(arrText.length !== 0)
-      input.innerHTML = arrText.join('');
-      else input.innerHTML = '0';
-    }
-    else if(btn.innerHTML === '='){
-      input.innerHTML = eval(input.innerHTML);     
-    }
-    else input.innerHTML += btn.innerHTML;
-  })
-})
-}
+  console.log(input);
+  var items = Array.from(document.querySelectorAll(".item"));
+  console.log(items);
+  items.forEach(function (btn) {
+    btn.addEventListener("click", function () {
+      if (input.innerHTML === "0") input.innerHTML = "";
+      if (btn.innerHTML === "AC") input.innerHTML = "0";
+      else if (btn.innerHTML === "DEL") {
+        var arrText = Array.from(input.innerHTML);
+        arrText.splice(arrText.length - 1, 1);
+        if (arrText.length !== 0) input.innerHTML = arrText.join("");
+        else input.innerHTML = "0";
+      } else if (btn.innerHTML === "=") {
+        input.innerHTML = eval(input.innerHTML);
+      } else input.innerHTML += btn.innerHTML;
+    });
+  });
+};
